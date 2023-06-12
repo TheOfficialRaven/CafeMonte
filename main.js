d4fe873cee6e7156548477c2f62cd99c3a18683a
TheOfficialRaven <134061451+TheOfficialRaven@users.noreply.github.com>
2023-06-12 15:14:09 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 function myNav() {
     let bar = document.querySelector(".bar");
     let nav = document.querySelector(".navigation");
+    let homeContent = document.querySelector(".home-content");
     bar.onclick = ()=>{
         if(nav.style.left == "0%"){
             nav.style.left = "-100%";
             bar.src = "Bars.png"
+            homeContent.style.left = "20%";
         }else{
             nav.style.left = "0%"
             bar.src = "X.png"
+            homeContent.style.left = "400px";
         }
     }
+    
 }
 myNav();
 
