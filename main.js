8d7e427c52e8cf912b5b0a7725aea093f4239e7d
TheOfficialRaven <134061451+TheOfficialRaven@users.noreply.github.com>
2023-06-13 15:28:14 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,19 +2,35 @@ function myNav() {
     let bar = document.querySelector(".bar");
     let nav = document.querySelector(".navigation");
     let homeContent = document.querySelector(".home-content");
+    let links = document.querySelectorAll(".navigation a");
+
     bar.onclick = ()=>{
         if(nav.style.left == "0%"){
-            nav.style.left = "-100%";
-            bar.src = "Bars.png"
-            homeContent.style.left = "10%";
+            closeNav();
         }else{
-            nav.style.left = "0%"
-            bar.src = "X.png"
-            homeContent.style.left = "50%";
+            openNav();
         }
     }
-    
+
+    function openNav() {
+        nav.style.left = "0%";
+        bar.src = "X.png";
+        homeContent.style.left = "50%";
+    }
+
+    function closeNav() {
+        nav.style.left = "-100%";
+        bar.src = "Bars.png";
+        homeContent.style.left = "10%";
+    }
+
+    links.forEach(link => {
+        link.addEventListener("click", ()=>{
+            closeNav();
+        });
+    });
 }
+
 myNav();
 
 window.onload = () => {
