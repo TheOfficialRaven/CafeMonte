23c9151173c3273849f8f3e91b27d3e4237ac01d
TheOfficialRaven <134061451+TheOfficialRaven@users.noreply.github.com>
2023-07-31 22:29:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
+///////////////////////////////////////////NavBar///////////////////////////////////////////////
 "use strict";
 function myNav() {
-    
-    
-  const bar = document.querySelector(".bar");
-  const nav = document.querySelector(".navigation");
-  const homeContent = document.querySelector(".home-content");
-  const links = document.querySelector(".navigation");
+  var bar = document.querySelector(".bar");
+  var nav = document.querySelector(".navigation");
+  var homeContent = document.querySelector(".home-content");
+  var links = document.querySelector(".navigation");
 
   bar.addEventListener("click", () => {
     if (nav.style.left === "0%") {
@@ -26,6 +25,10 @@ function myNav() {
     bar.src = "Bars.png";
     homeContent.style.left = "2.5%";
   }
+    
+    function isNavOpen() {
+  return nav.style.left === "0%";
+}
 
   links.addEventListener("click", (event) => {
     const target = event.target;
@@ -39,6 +42,9 @@ function myNav() {
     if (window.pageYOffset > 0) {
       barsImg.style.display = "none";
       nav.classList.add("scrolled");
+         if (isNavOpen()) {
+        closeNav();
+    }
     } else {
       barsImg.style.display = "block";
       nav.classList.remove("scrolled");
@@ -49,27 +55,15 @@ function myNav() {
 myNav();
 
 window.onload = () => {
-  const homeContent = document.querySelector(".home-content");
-  homeContent.style.left = "50px";
+  setTimeout(() => {
+    const homeContent = document.querySelector(".home-content");
+    homeContent.classList.remove("initial-position"); // Remove the 'initial-position' class
+    homeContent.style.left = "50px"; // Move it to the final position
+  }, 500); // 500 milliseconds (0.5 seconds) delay
 };
 
-// Smooth scrolling polyfill for older browsers
-$(document).ready(function () {
-  // Check if the browser doesn't support smooth scrolling natively
-  if (!("scrollBehavior" in document.documentElement.style)) {
-    // Add smooth scrolling behavior to anchor links
-    $(".scroll-link").on("click", function (event) {
-      event.preventDefault();
-      $("html, body").animate(
-        {
-          scrollTop: $($.attr(this, "href")).offset().top,
-        },
-        500 // Adjust the duration (in milliseconds) as desired
-      );
-    });
-  }
-});
 
+////////////////////////////////// Logo Home Button////////////////////////////////////////////////
  // Get the image element by its ID
   var logoImage = document.getElementById('logo-image');
 
@@ -78,3 +72,46 @@ $(document).ready(function () {
     // Redirect the user to the home page
     window.location.href = '#'; // Replace 'https://example.com' with your actual home page URL
   });
+
+//////////////////////////////Modal/////////////////////////////////////////////////////////////
+var modal = document.getElementById("modalDialog");
+
+// Get the button that opens the modal
+var btn = document.getElementById("contactBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the button is clicked, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+// When the <span> (close button) is clicked, close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event){
+    var form = document.getElementsByClassName("medal-content");
+    if(event.target == form){
+        modal.style.display = "none";
+    }
+};
+
+// Optional: Close the modal if the user presses the Escape key
+window.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        modal.style.display = "none";
+    }
+});
+
+
+
+
+
+
+
+
+
