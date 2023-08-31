b5b6715fafc851eee4501f608f2068dfa5d36b87
TheOfficialRaven <134061451+TheOfficialRaven@users.noreply.github.com>
2023-08-31 12:28:17 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 ///////////////////////////////////////////NavBar///////////////////////////////////////////////
 "use strict";
-function myNav() {
-  var bar = document.querySelector(".bar");
-  var nav = document.querySelector(".navigation");
-  var homeContent = document.querySelector(".home-content");
-  var links = document.querySelector(".navigation");
 
-  bar.addEventListener("click", () => {
-    if (nav.style.left === "0%") {
-      closeNav();
-    } else {
-      openNav();
-    }
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  const bar = document.querySelector(".bar");
+  const nav = document.querySelector(".navigation");
+  const homeContent = document.querySelector(".home-content");
+  const links = document.querySelector(".navigation");
+  const barsImg = document.querySelector(".bar");
+  const logoImage = document.getElementById("logo-image");
 
   function openNav() {
     nav.style.left = "0%";
@@ -25,10 +20,18 @@ function myNav() {
     bar.src = "Bars.png";
     homeContent.style.left = "2.5%";
   }
-    
-    function isNavOpen() {
-  return nav.style.left === "0%";
-}
+
+  function isNavOpen() {
+    return nav.style.left === "0%";
+  }
+
+  bar.addEventListener("click", () => {
+    if (isNavOpen()) {
+      closeNav();
+    } else {
+      openNav();
+    }
+  });
 
   links.addEventListener("click", (event) => {
     const target = event.target;
@@ -36,79 +39,33 @@ function myNav() {
       closeNav();
     }
   });
-    
-////////////////////////////////// Hide bar on scroll ////////////////////////////////////////
 
   window.addEventListener("scroll", () => {
-    const barsImg = document.querySelector(".bar");
     if (window.pageYOffset > 0) {
       barsImg.style.display = "none";
       nav.classList.add("scrolled");
-         if (isNavOpen()) {
+      if (isNavOpen()) {
         closeNav();
-    }
+      }
     } else {
       barsImg.style.display = "block";
       nav.classList.remove("scrolled");
     }
   });
-}
 
-myNav();
-
-setTimeout(() => {
-  const homeContent = document.querySelector(".home-content");
-  homeContent.classList.remove("initial-position");
-  homeContent.style.left = "38px";
-}, 300);
-
-////////////////////////////////// Logo Home Button////////////////////////////////////////////////
- // Get the image element by its ID
-  var logoImage = document.getElementById('logo-image');
-
-  // Add a click event listener to the image
-  logoImage.addEventListener('click', function() {
-    // Redirect the user to the home page
-    window.location.href = '#'; // Replace 'https://example.com' with your actual home page URL
+///////////////////////////////////////////Logo Home btn ////////////////////////////////////////////    
+  logoImage.addEventListener("click", function () {
+    window.location.href = "#"; // Replace with your actual home page URL
   });
 
-//////////////////////////////Modal/////////////////////////////////////////////////////////////
-var modal = document.getElementById("modalDialog");
-
-// Get the button that opens the modal
-var btn = document.getElementById("contactBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-var header = document.getElementsByClassName("header");
-
-// When the button is clicked, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-};
-
-// When the <span> (close button) is clicked, close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event){
-    var form = document.getElementsByClassName("medal-content");
-    if(event.target == form){
-        modal.style.display = "none";
-    }
-};
-
-// Optional: Close the modal if the user presses the Escape key
-window.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-        modal.style.display = "none";
-    }
+    
+    
+//////////////////////////////////////////Home content from left //////////////////////////////////
+    
+  setTimeout(() => {
+    homeContent.classList.remove("initial-position");
+    homeContent.style.left = "38px";
+  }, 300);
 });
-
-
-
 
 
